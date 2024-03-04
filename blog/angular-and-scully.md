8581865ee4da62227a726a1e89d6f9cdb3c83efc
CampoSuarezJavierDavidCampus <campo.javier1502@gmail.com>
2024-03-04 07:59:29 -0500

    feat: :sparkles: I Create a new post with Scullyio commands "ng g @scullyio/add-post --name=<<titile>>"

---
+++ blog/angular-and-scully.md
@@ -0,0 +1,13 @@
+---
+title: Angular and Scully
+description: blog description
+published: false
+---
+
+# Angular and Scully
+Angular is a robust Javascript framework that we can use to build exellent and performant web applications.
+Scully is a popular static website generator that empowers the Angular framework with Jamstack characteristics.
+You can find more about them in the following links:
+- https://angular.io
+- https://scully.io
+- https://www.jamstack.org
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable, map } from 'rxjs';
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+  post$:Observable<ScullyRoute[]> | undefined;
+
+  //* We are going to add all post trouth lezzy loadding
+  constructor(
+    private scullyService:ScullyRoutesService
+  ) {}
+  ngOnInit(): void {
+    this.post$ = this.scullyService.available$.pipe(
+      map(post => post.filter(post => post.title))
+    );
+  }
 
 }
