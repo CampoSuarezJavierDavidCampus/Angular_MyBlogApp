e08588e87f20d5f28bf9a20d91620b5ca9fbb624
CampoSuarezJavierDavidCampus <campo.javier1502@gmail.com>
2024-03-04 05:53:54 -0500

    feat: :sparkles: Add Contact route and implement it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ContactComponent } from './contact/contact.component';
+const routes: Routes = [
+  {path:'contact',component:ContactComponent}//TODO Add Contact Component To Router
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ContactComponent } from './contact/contact.component';
+import { ContactComponent } from './contact.component';
+
 
 
 
